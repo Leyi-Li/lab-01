00a5eea5f55b8ee740511d93161e0e78f4f60f3d
leyla li <leylali@qq.com>
2019-07-15 16:21:18 -0700

    changed colors for another breakpoint

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 'use strict';
 
 function colorChange(){
-  if($(window).width() > 768){
-    $('.box-1').css('background-color','#FFC09F');
-    $('.box-2').css('background-color','#FFEE93');
-    $('.box-3').css('background-color','#FCF5C7');
-    $('.box-4').css('background-color','#A0CED9');
-    $('.box-5').css('background-color','#ADF7D6');
-    $('.box-6').css('background-color','#6EF9F5');
-    $('.box-7').css('background-color','#63E2C6');
-    $('.box-8').css('background-color','#DEC5E3');
-    $('.box-9').css('background-color','#B6DCFE');
+  if($(window).width() > 961){
+    $('.box-a').css('background-color','#FFC09F');
+    $('.box-b').css('background-color','#FFEE93');
+    $('.box-c').css('background-color','#FCF5C7');
+    $('.box-d').css('background-color','#A0CED9');
+    $('.box-e').css('background-color','#ADF7D6');
+    $('.box-f').css('background-color','#6EF9F5');
+    $('.box-g').css('background-color','#63E2C6');
+    $('.box-h').css('background-color','#DEC5E3');
+    $('.box-i').css('background-color','#B6DCFE');
+  }else if(960 > $(window).width() && $(window).width() >768 ){
+    $('.box-a').css('background-color','#D4C5E2');
+    $('.box-b').css('background-color','#C9D7F8');
+    $('.box-c').css('background-color','#A7E2E3');
+    $('.box-d').css('background-color','#80CFA9');
+    $('.box-e').css('background-color','#4C6663');
+    $('.box-f').css('background-color','#42F2F7');
+    $('.box-g').css('background-color','#46ACC2');
+    $('.box-h').css('background-color','#EA638C');
+    $('.box-i').css('background-color','#B33C86');
   }else{
-    $('.box-1').css('background-color','aqua');
-    $('.box-2').css('background-color','rgb(23, 116, 116)');
-    $('.box-3').css('background-color','rgb(16, 73, 126)');
-    $('.box-4').css('background-color','rgb(13, 18, 87)');
-    $('.box-5').css('background-color','rgb(109, 18, 184)');
-    $('.box-6').css('background-color','rgb(57, 13, 107)');
-    $('.box-7').css('background-color','rgb(221, 40, 167)');
-    $('.box-8').css('background-color','rgb(219, 23, 131)');
-    $('.box-9').css('background-color','rgb(180, 32, 52)');  
+    $('.box-a').css('background-color','aqua');
+    $('.box-b').css('background-color','rgb(23, 116, 116)');
+    $('.box-c').css('background-color','rgb(16, 73, 126)');
+    $('.box-d').css('background-color','rgb(13, 18, 87)');
+    $('.box-e').css('background-color','rgb(109, 18, 184)');
+    $('.box-f').css('background-color','rgb(57, 13, 107)');
+    $('.box-g').css('background-color','rgb(221, 40, 167)');
+    $('.box-h').css('background-color','rgb(219, 23, 131)');
+    $('.box-i').css('background-color','rgb(180, 32, 52)');  
   }
 }
 
